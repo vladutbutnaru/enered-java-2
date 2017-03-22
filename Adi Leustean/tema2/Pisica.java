87d66f5eabe3d4efbece56daa9ddaf4986bbf86c
Adi Leustean <leustean.adi.eduard@gmail.com>
2017-03-22 07:08:24 +0200

    Create Pisica.java

---
+++ Adi Leustean/tema2/Pisica.java
@@ -0,0 +1,30 @@
+public class Pisica extends Animal implements Callable {
+
+	private String prop;
+	private String prop1;
+
+	public String getProp() {
+		return prop;
+	}
+
+	public void setProp(String prop) {
+		this.prop = prop;
+	}
+
+	public String getProp1() {
+		return prop1;
+	}
+
+	public void setProp1(String prop1) {
+		this.prop1 = prop1;
+	}
+
+	@Override
+	public void call() {
+		System.out.println(">.<");
+
+		System.out.println(getVorbeste());
+		System.out.println();
+	}
+
+}
