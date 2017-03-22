f69169befe2765cfaf3050b26046258c4f1bfe04
Adi Leustean <leustean.adi.eduard@gmail.com>
2017-03-22 07:07:59 +0200

    Create Main.java

---
+++ Adi Leustean/tema2/Main.java
@@ -0,0 +1,50 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Pisica cartof=new Pisica();
+		cartof.setProp("apare des pe la bloc");
+		cartof.setNume("Cartof");
+		cartof.setProp1("sta la flocosit");
+		cartof.setVorbeste(" mau mau");
+		cartof.call();
+		
+		Caine kira =new Caine();
+		kira.setNume("Kira");
+		kira.setProp("mananca de sparge!");
+		kira.setProp1(" bate cuba!");
+		kira.setVorbeste("hahm, hahm");
+		kira.call();
+		
+		
+		Cal ginger= new Cal();
+		ginger.setVorbeste("iiiha, pfffrp");
+		ginger.setNume("Ginger");
+		ginger.setProp("papa mere rosii");
+		ginger.setProp1("e potcovit");
+		ginger.call();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
